51066a76a94a699f15cc2a5ea11025996c125d8a
Hendrik Sollich <hendrik@hoodie.de>
2024-10-10 20:21:40 +0200

    WIP

--- src/actor.rs
+++ src/actor.rs
@@ -53,7 +53,6 @@ mod service {
             } else {
                 let (event_loop, addr) = Environment::unbounded().launch(Self::default());
                 let mut joiner = S::spawn(event_loop.map(|fut| fut.unwrap()));
-                let actor = joiner.join().await;
                 Ok(addr)
             }
         }
@@ -67,6 +66,7 @@ mod spawn_strategy {
     pub type JoinFuture<A> = Pin<Box<dyn Future<Output = Option<A>> + Send>>;
 
     pub(crate) trait Joiner<A: Actor>: Send + Sync {
+        #[allow(unused)]
         fn join(&mut self) -> JoinFuture<A>;
     }
 
@@ -109,5 +109,86 @@ mod spawn_strategy {
                 })
             })
         }
+    }
+
+    #[cfg(feature = "async-std")]
+    pub struct AsyncStdSpawner;
+    impl<A: Actor> Spawner<A> for AsyncStdSpawner {
+        fn spawn<F>(future: F) -> Box<dyn Joiner<A>>
+        where
+            F: Future<Output = A> + Send + 'static,
+        {
+            let handle = Arc::new(async_lock::Mutex::new(Some(async_std::task::spawn(future))));
+            Box::new(move || -> JoinFuture<A> {
+                let handle = Arc::clone(&handle);
+                Box::pin(async move {
+                    let mut handle: Option<async_std::task::JoinHandle<A>> =
+                        handle.lock().await.take();
+
+                    if let Some(handle) = handle.take() {
+                        Some(handle.await)
+                    } else {
+                        None
+                    }
+                })
+            })
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use service::Service;
+    #[allow(unused)]
+    use spawn_strategy::{AsyncStdSpawner, TokioSpawner};
+
+    use crate::{Handler, Message};
+
+    use super::*;
+
+    #[tokio::test]
+    async fn get_service_from_registry() {
+
+        struct Ping;
+        struct Pong;
+        impl Message for Ping {
+            type Result = Pong;
+        }
+
+
+        #[derive(Debug, Default)]
+        struct AsyncStdServiceActor;
+        impl Actor for AsyncStdServiceActor {}
+        impl Service<AsyncStdSpawner> for AsyncStdServiceActor {}
+        impl Handler<Ping> for AsyncStdServiceActor {
+            async fn handle(&mut self, _ctx: &mut Context<Self>, _msg: Ping) -> Pong {
+                Pong
+            }
+        }
+
+        #[derive(Debug, Default)]
+        struct TokioServiceActor;
+        impl Actor for TokioServiceActor {}
+        impl Service<TokioSpawner> for TokioServiceActor {}
+        impl Handler<Ping> for TokioServiceActor {
+            async fn handle(&mut self, _ctx: &mut Context<Self>, _msg: Ping) -> Pong {
+                Pong
+            }
+        }
+
+        let mut addr_asyncstd = AsyncStdServiceActor::from_registry().await.unwrap();
+        assert!(!addr_asyncstd.stopped());
+
+        let mut addr_tokio = TokioServiceActor::from_registry().await.unwrap();
+        assert!(!addr_tokio.stopped());
+
+
+        addr_asyncstd.call(Ping).await.unwrap();
+        addr_asyncstd.stop().unwrap();
+        addr_asyncstd.await.unwrap();
+
+        addr_tokio.call(Ping).await.unwrap();
+        addr_tokio.stop().unwrap();
+        addr_tokio.await.unwrap()
     }
 }
